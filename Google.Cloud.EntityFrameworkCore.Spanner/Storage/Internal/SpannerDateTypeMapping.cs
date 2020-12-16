1896e146e6234038df53cfb4afafb8b678bc76e5
olavloite <koloite@gmail.com>
2020-12-16 18:48:48 +0100

    feat: add numeric array + date mapping

---
+++ Google.Cloud.EntityFrameworkCore.Spanner/Storage/Internal/SpannerDateTypeMapping.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2020, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Spanner.Data;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+
+namespace Google.Cloud.EntityFrameworkCore.Spanner.Storage.Internal
+{
+    public class SpannerDateTypeMapping : RelationalTypeMapping
+    {
+        public SpannerDateTypeMapping() : base(SpannerDbType.Date.ToString(), typeof(DateTime), System.Data.DbType.Date) { }
+
+        protected SpannerDateTypeMapping(RelationalTypeMappingParameters parameters)
+            : base(parameters) { }
+
+        protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
+            => new SpannerDateTypeMapping(parameters);
+
+        protected override string GenerateNonNullSqlLiteral(object value)
+            => FormattableString.Invariant($"DATE '{(DateTime)value:yyyy-MM-dd}'");
+    }
+}
