e3e63ad17faaa3a5080d7b2d3eb28afc38a169d5
Knut Olav Løite <koloite@gmail.com>
2020-12-29 06:38:47 +0100

    test: add tests for lazy loading (#10)
    * test: add tests for lazy loading
    
    * fix: remove unused code

--- Google.Cloud.EntityFrameworkCore.Spanner.IntegrationTests/ScaffoldingTests.cs
+++ Google.Cloud.EntityFrameworkCore.Spanner.IntegrationTests/ScaffoldingTests.cs
@@ -15,9 +15,6 @@
 using Google.Cloud.EntityFrameworkCore.Spanner.IntegrationTests.Model;
 using Google.Cloud.Spanner.Data;
 using System;
-using Google.Cloud.Spanner.Common.V1;
-using Google.Cloud.Spanner.Data;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using Xunit;
 
@@ -137,6 +134,7 @@ namespace Google.Cloud.EntityFrameworkCore.Spanner.IntegrationTests
                 // Reget the album from the database.
                 var album = await db.Albums.FindAsync(1L);
                 Assert.Equal("Pete Henderson's first album", album.Title);
+                Assert.NotNull(album.Singer);
             }
         }
 
@@ -175,9 +173,8 @@ namespace Google.Cloud.EntityFrameworkCore.Spanner.IntegrationTests
                 db.Tracks.Add(track);
                 await db.SaveChangesAsync();
 
-                // Check that we can use the AlbumNavigation.
-                // TODO: Implement references based on interleaved tables.
-                // Assert.Equal("Allis Morrison's second album", track.AlbumNavigation.Title);
+                // Check that we can use the Album reference.
+                Assert.Equal("Allis Morrison's second album", track.Album.Title);
             }
 
             using (var db = new TestSpannerSampleDbContext(_fixture.DatabaseName))
@@ -185,6 +182,93 @@ namespace Google.Cloud.EntityFrameworkCore.Spanner.IntegrationTests
                 // Reget the track from the database.
                 var track = await db.Tracks.FindAsync(2L, 1L);
                 Assert.Equal("Track 1", track.Title);
+                // Check that the link with Album works.
+                Assert.NotNull(track.Album);
+                Assert.Equal("Allis Morrison's second album", track.Album.Title);
+            }
+        }
+
+        [Fact]
+        public async void CanInsertConcertAndPerformances()
+        {
+            using (var db = new TestSpannerSampleDbContext(_fixture.DatabaseName))
+            {
+                var singer = new Singers
+                {
+                    SingerId = 4,
+                    FirstName = "Rob",
+                    LastName = "Morrison",
+                };
+                db.Singers.Add(singer);
+
+                var album = new Albums
+                {
+                    SingerId = singer.SingerId,
+                    AlbumId = 4,
+                    Title = "Rob Morrison's first album",
+                };
+                db.Albums.Add(album);
+
+                var track = new Tracks
+                {
+                    AlbumId = album.AlbumId,
+                    TrackId = 1,
+                    Title = "Rob Morrison's first track",
+                };
+                db.Tracks.Add(track);
+
+                var venue = new Venues
+                {
+                    Code = "PARK",
+                    Name = "Central Park",
+                };
+                db.Venues.Add(venue);
+
+                var concert = new Concerts
+                {
+                    VenueCode = "PARK",
+                    StartTime = new DateTime(2020, 12, 28, 10, 0, 0),
+                    SingerId = 4,
+                    Title = "End of year concert",
+                };
+                db.Concerts.Add(concert);
+
+                var performance = new Performances
+                {
+                    VenueCode = concert.VenueCode,
+                    ConcertStartTime = concert.StartTime,
+                    SingerId = concert.SingerId,
+                    StartTime = concert.StartTime.AddHours(1),
+                    AlbumId = track.AlbumId,
+                    TrackId = track.TrackId,
+                    Rating = 9.8D,
+                };
+                db.Performances.Add(performance);
+                await db.SaveChangesAsync();
+            }
+            using (var db = new TestSpannerSampleDbContext(_fixture.DatabaseName))
+            {
+                // Reget the concert from the database.
+                var refreshedConcert = await db.Concerts.FindAsync("PARK", new DateTime(2020, 12, 28, 10, 0, 0), 4L);
+                Assert.Equal("End of year concert", refreshedConcert.Title);
+                // Check that the concert turns up in the collections of other entities.
+                var singer = await db.Singers.FindAsync(4L);
+                Assert.Collection(singer.Concerts, c => c.Equals(refreshedConcert));
+                Assert.Equal(1, singer.Concerts.Count);
+                var venue = await db.Venues.FindAsync("PARK");
+                Assert.Collection(venue.Concerts, c => c.Equals(refreshedConcert));
+                Assert.Equal(1, venue.Concerts.Count);
+
+                // Check the track
+                var track = await db.Tracks.FindAsync(4L, 1L);
+                Assert.Equal("Rob Morrison's first track", track.Title);
+
+                // Reget the performance from the database.
+                var performance = await db.Performances.FindAsync(venue.Code, singer.SingerId, refreshedConcert.StartTime.AddHours(1));
+                Assert.Equal(9.8D, performance.Rating);
+                Assert.NotNull(performance.Tracks);
+                Assert.NotNull(performance.Tracks.Album);
+                Assert.Equal("Rob Morrison's first album", performance.Tracks.Album.Title);
             }
         }
     }
