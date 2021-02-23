ba8056c28f8105e769d5fd56db630d2b006cb12a
Lalji Kanjareeya <lalji.kanjareeya@qlogic.io>
2021-02-23 18:08:45 +0530

    feat: migration builder extension

---
+++ Google.Cloud.EntityFrameworkCore.Spanner/Extensions/SpannerMigrationBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2021, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.EntityFrameworkCore.Spanner.Infrastructure.Internal;
+using JetBrains.Annotations;
+using System;
+using System.Reflection;
+
+namespace Microsoft.EntityFrameworkCore.Migrations
+{
+    /// <summary>
+    ///     Spanner specific extension methods for <see cref="MigrationBuilder" />.
+    /// </summary>
+    public static class SpannerMigrationBuilderExtensions
+    {
+        /// <summary>
+        ///     <para>
+        ///         Returns true if the database provider currently in use is the Spanner provider.
+        ///     </para>
+        /// </summary>
+        /// <param name="migrationBuilder">
+        ///     The migrationBuilder from the parameters on <see cref="Migration.Up(MigrationBuilder)" /> or
+        ///     <see cref="Migration.Down(MigrationBuilder)" />.
+        /// </param>
+        /// <returns> True if Spanner is being used; false otherwise. </returns>
+        public static bool IsSpanner([NotNull] this MigrationBuilder migrationBuilder)
+            => string.Equals(
+                migrationBuilder.ActiveProvider,
+                typeof(SpannerOptionsExtension).GetTypeInfo().Assembly.GetName().Name,
+                StringComparison.Ordinal);
+    }
+}
