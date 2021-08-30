dd6a8100193a8aa03d877c9be668d2bb1a41461e
Knut Olav Løite <koloite@gmail.com>
2021-08-30 18:32:42 +0200

    test: disabel parallel running of unit tests

--- Google.Cloud.EntityFrameworkCore.Spanner.Tests/MigrationTests/MigrationMockServerTests.cs
+++ Google.Cloud.EntityFrameworkCore.Spanner.Tests/MigrationTests/MigrationMockServerTests.cs
@@ -12,12 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Google.Cloud.EntityFrameworkCore.Spanner.Tests.MigrationTests;
 using Google.Cloud.Spanner.Admin.Database.V1;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
-namespace Google.Cloud.EntityFrameworkCore.Spanner.Tests
+namespace Google.Cloud.EntityFrameworkCore.Spanner.Tests.MigrationTests
 {
     /// <summary>
     /// Tests migrations using an in-mem Spanner mock server.
