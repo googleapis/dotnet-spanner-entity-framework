ad02a9d0596d7562fd67aed0161948080ef404da
Amanda Tarafa Mas <atarafamas@google.com>
2026-02-12 22:36:23 -0800

    chore(build): Support automatic releases for the new package
    See b/453564547

--- build.sh
+++ build.sh
@@ -5,15 +5,40 @@ set -e
 export ContinuousIntegrationBuild=true
 export Configuration=Release
 
-echo Building...
+echo Building Google.Cloud.EntityFrameworkCore.Spanner...
 dotnet build -nologo -clp:NoSummary -v quiet Google.Cloud.EntityFrameworkCore.Spanner
 
 echo Testing...
-dotnet test -nologo --no-build -v quiet Google.Cloud.EntityFrameworkCore.Spanner.Tests
+dotnet test -nologo -v quiet Google.Cloud.EntityFrameworkCore.Spanner.Tests
 
 echo Packing...
 rm -rf nupkg
 dotnet pack -nologo -v quiet Google.Cloud.EntityFrameworkCore.Spanner -o $PWD/nupkg
 
+echo Building spanner-ado-net...
+pushd spanner-ado-net/spanner-ado-net
+
+# Ensure go-sql-spanner is present if not skipped by CI
+if [ ! -d "go-sql-spanner" ]; then
+  source spanner-lib-version.sh
+  git clone https://github.com/googleapis/go-sql-spanner.git --branch "$SPANNER_LIB_BRANCH" go-sql-spanner
+fi
+
+chmod +x build-binaries.sh
+./build-binaries.sh true
+
+echo Building dotnet project...
+dotnet build -c Release spanner-ado-net.csproj
+
+echo Testing ADO.NET driver...
+pushd ..
+dotnet test -nologo spanner-ado-net-tests
+dotnet test -nologo spanner-ado-net-specification-tests
+popd
+
+echo Packing...
+dotnet pack -c Release spanner-ado-net.csproj -o ../../nupkg
+
 echo Created packages:
-ls nupkg
+ls ../../nupkg
+popd
