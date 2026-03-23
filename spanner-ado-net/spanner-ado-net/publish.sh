47fe32d46364385d26dff36a67374b2fecf270f4
Knut Olav Løite <koloite@gmail.com>
2026-03-23 11:35:23 +0100

    fix(ado.net): trigger release of ADO.NET

--- spanner-ado-net/spanner-ado-net/publish.sh
+++ spanner-ado-net/spanner-ado-net/publish.sh
@@ -1,4 +1,4 @@
-VERSION=$(date -u +"1.0.0-alpha.%Y%m%d%H%M%S")
+VERSION=$(date -u +"1.0.0-rc-%Y%m%d%H%M%S")
 
 echo "Publishing as version $VERSION"
 sed -i "" "s|<Version>.*</Version>|<Version>$VERSION</Version>|g" spanner-ado-net.csproj
